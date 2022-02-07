d50efe5fe307461e30a91c59fdebc23917cf7ab1
BitDaMit <adrian.gilca.ax@valahia.ro>
2022-02-07 21:01:50 +0200

    initial commit

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public Interactible focus;
+
+    public LayerMask movementMask;
+
+    Camera cam;
+    PlayerMotor motor;
+
+    public static bool isMovable;
+    void Start()
+    {
+        isMovable = true;
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
+        if (isMovable)
+        {
+       if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 100))
+            {
+                motor.MoveToPoint(hit.point);
+
+                RemoveFocus();
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+
+               Interactible interactible = hit.collider.GetComponent<Interactible>();
+            if (interactible != null)
+                {
+                    SetFocus(interactible);
+                }
+
+            }
+        }
+        }
+    }
+
+    void SetFocus(Interactible newFocus)
+    {
+        if (newFocus != focus)
+        {
+            if(focus != null)
+            focus.OnDefocused();
+
+            focus = newFocus;
+            motor.FollowTarget(newFocus);
+        }
+
+        
+        newFocus.OnFocused(transform);
+        
+    }
+
+    void RemoveFocus()
+    {
+        if (focus != null)
+            focus.OnDefocused();
+
+        focus = null;
+        motor.StopFollowingTarget();
+    }
+}
---
+++ Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : CharacterStats
+{
+
+    public GameObject deathText;
+    void Start()
+    {
+        EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;
+    }
+
+    void OnEquipmentChanged (Equipment newItem, Equipment oldItem)
+    {
+        if (newItem != null)
+        {
+            armor.AddModifier(newItem.armorModifier);
+            damage.AddModifier(newItem.damageModifier);
+        }
+
+        if (oldItem != null)
+        {
+            armor.RemoveModifier(oldItem.armorModifier);
+            damage.RemoveModifier(oldItem.damageModifier);
+        }
+
+    }
+
+    public override void Die()
+    {
+        deathText.gameObject.SetActive(true);
+        Debug.Log("test");
+        StartCoroutine(deathScreen());
+      
+    }
+    IEnumerator deathScreen()
+    {
+        PlayerController.isMovable = false;
+        yield return new WaitForSeconds(2f);
+        base.Die();
+        PlayerManager.instance.KillPlayer();
+        PlayerController.isMovable = true;
+    }
+}
